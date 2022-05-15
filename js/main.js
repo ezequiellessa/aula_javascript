9c02ff5a0149c31b3bc30585c18360781b669d17
Ezequiel Lessa de oliveira <ezequiellessa@gmail.com>
2022-05-15 07:31:25 -0300

    aula de java script

---
+++ js/main.js
@@ -0,0 +1,132 @@
+
+
+
+
+
+function clicou() {
+   document.getElementById("agradecimento").innerText = "OBRIGADO ESTOU APRENDENDO";
+
+
+
+
+
+
+
+
+  // console.log(document.getElementById("agradecimento"));
+    
+}
+
+
+
+
+/*var validar = 0;
+function validaidade(idade) {
+    
+    if(idade >= 18){
+        validar = true
+    }
+    else{
+        validar = false
+       
+    }
+return validar;
+}
+
+var idade = prompt("qual é a sua idade")
+validaidade(idade);
+
+console.log(validar);
+
+*/
+
+
+
+
+//alert(soma(5 , 10));
+
+//aula 3
+
+
+/*var d = new Date(); amortrar a data 
+alert(d);
+console.log(d); */
+
+
+/*var count;
+for(count= 0; count <= 10; count++){ //contagem com for
+    console.log(count);
+    alert(count);
+}
+
+*/
+
+
+
+/*var count = 0; //contagem com while
+while (count <= 5){
+    console.log(count);
+    count++;
+    alert(count);
+
+}
+
+
+
+
+
+/*var idade = prompt("qual é sua idade?"); prompt pergunta sua idade e informa q vc e maior de idade usando if = se e else = se não
+if(idade >= 18){
+    alert("vc é maior de idade");}
+    else{alert("vc é menor de idade");}
+    */
+
+/*var idade=18;
+if(idade <= 18){
+    alert("vc é maior de idade");}
+    else{alert("vc é menor de idade");}
+*/
+
+
+
+// <<<<<<< segunda aula java script >>>>>>>>>>>>>>>>>>>
+
+//var frutas = ["maçã","pera","laranja"];
+//frutas.push("cacau");adicionar
+//frutas.pop();retirar o ultimo
+//console.log(frutas);
+//console.log(frutas.length);saber quantidade
+//console.log(frutas.reverse());reverter a lista de frutas
+//console.log(frutas.toString());deixa em string com virgula
+//console.log(frutas.join("-"));deixa separado com -
+
+/*
+var fruta={nome:"maçã",cor:"vermelha"} dicionario
+console.log(fruta.cor)
+console.log(fruta.nome)
+*/
+
+//var fruta =[{nome:"maçã",cor:"vermelha"},{nome:"uva",cor:"roxa"}]
+//console.log(fruta);
+//alert(fruta[1].nome);
+
+
+
+// <<<<<<< primeira aula java script >>>>>>>>>>>>>>>>>>>
+//var nome = "ezequiel";
+//var idade = 35;
+//var idade2 = 10;
+//var n1 = 35;
+//var n2 = 10;
+//var frase="japão é o melhor time do mundo";
+//alert( nome + "tem" +  idade + "anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade);
+//console.log(idade2);
+//console.log(frase.replace("japão","brasil"));trocar nome japão para brasil
+//console.log(frase.toUpperCase()); colocar letras maiuscula
+//console.log(frase.toLowerCase());colocar letras menusculas
+//console.log(n1 * n2); multiplicação
+//console.log(n1 / n2); divisão
+
